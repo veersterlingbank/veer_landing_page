734779deb353382134975bdbc6398909f5da3b61
emodot <emayeodavid@gmail.com>
2024-12-20 09:54:02 +0100

    removed animations on mobile

--- src/variants.js
+++ src/variants.js
@@ -1,21 +1,24 @@
 import { easeIn } from "framer-motion";
 
 export const fadeIn = (direction, delay) => {
-  return {
-    hidden: {
-      y: direction === "up" ? 40 : direction === "down" ? -40 : 0,
-      x: direction === "left" ? 40 : direction === "right" ? -40 : 0,
-    },
-    show: {
-      y: 0,
-      x: 0,
-      opacity: 1,
-      transition: {
-        type: "spring",
-        duration: 1.2,
-        delay: delay,
-        ease: easeIn,
+  const isMobile = window.innerWidth < 768;
+  if (!isMobile) {
+    return {
+      hidden: {
+        y: direction === "up" ? 40 : direction === "down" ? -40 : 0,
+        x: direction === "left" ? 40 : direction === "right" ? -40 : 0,
       },
-    },
-  };
+      show: {
+        y: 0,
+        x: 0,
+        opacity: 1,
+        transition: {
+          type: "spring",
+          duration: 1.2,
+          delay: delay,
+          ease: easeIn,
+        },
+      },
+    };
+  }
 };
